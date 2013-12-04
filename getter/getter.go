01b6466ca15ac00bf470feb44d9a1ac0f5a64216
Matchu <matchu1993@gmail.com>
2013-12-03 21:29:47 -0600

    extract maxTries into a func

--- getter/getter.go
+++ getter/getter.go
@@ -8,12 +8,17 @@ import (
 	"strconv"
 )
 
-func main() {
+func maxTries() uint64 {
 	flag.Parse()
 	maxTries, err := strconv.ParseUint(flag.Arg(0), 10, 64)
 	if err != nil {
 		maxTries = 1
 	}
+	return maxTries
+}
+
+func main() {
+	maxTries := maxTries()
 	notable, ok := source.GetNotable(maxTries)
 	if !ok {
 		log.Fatalf("failed after %d tries\n", maxTries)
