f262fad9bb59f89c41ac3b2fab0110c12848a77d
Matchu <matchu1993@gmail.com>
2013-12-03 20:49:36 -0600

    initial commit: basic getter functionality

---
+++ getter/getter.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"openneo.net/notables/source"
+)
+
+func main() {
+	fmt.Println(source.GetNotable(64))
+}
---
+++ source/source.go
@@ -0,0 +1,64 @@
+package source
+
+import (
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+var (
+	petNamePattern = regexp.MustCompile(
+		`/petlookup\.phtml\?pet=([a-zA-Z0-9_]+)`)
+	imageHashPattern = regexp.MustCompile(
+		`http://pets\.neopets\.com/cp/([a-z0-9]+)/1/2\.png`)
+)
+
+type Notable struct {
+	petName   string
+	imageHash string
+}
+
+func GetNotable(maxTries int) (Notable, bool) {
+	for i := 0; i < maxTries; i++ {
+		notable := requestNotable()
+		if notable.petName != "" {
+			return notable, true
+		}
+	}
+	return Notable{}, false
+}
+
+func requestNotable() Notable {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "http://www.neopets.com", nil)
+	if err != nil {
+		return Notable{}
+	}
+	req.Header.Add("User-Agent", "OpenNeo Notables Tracker")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return Notable{}
+	}
+
+	// CONSIDER: match on a buffer instead of loading in the whole string?
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return Notable{}
+	}
+	resp.Body.Close()
+	body := string(bodyBytes)
+
+	petNameMatch := petNamePattern.FindStringSubmatch(body)
+	if len(petNameMatch) < 2 {
+		return Notable{}
+	}
+
+	imageHashMatch := imageHashPattern.FindStringSubmatch(body)
+	if len(imageHashMatch) < 2 {
+		return Notable{}
+	}
+
+	return Notable{petNameMatch[1], imageHashMatch[1]}
+}
