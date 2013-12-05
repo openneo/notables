eca928e39abfeffb7d06322b6f22e5c1f351fc19
Matchu <matchu1993@gmail.com>
2013-12-04 22:53:45 -0600

    add server port as a flag

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/openneo/neopets-notables-go/db"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 
 func main() {
 	db.SetupFlag()
+	port := flag.Int("port", 8888, "port on which to run web server")
 	flag.Parse()
 
 	session, err := db.Connect()
@@ -18,5 +20,5 @@ func main() {
 
 	http.HandleFunc("/api/1/days/ago/", makeHandler(handleDayAgo, session))
 	http.HandleFunc("/api/1/days/", makeHandler(handleExactDay, session))
-	http.ListenAndServe(":8888", nil)
+	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 }
